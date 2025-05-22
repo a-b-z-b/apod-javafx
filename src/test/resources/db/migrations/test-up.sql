80c65c0fdf63596d9e8c74fe3bbc6ee1284cc1fe
zZ7_aki <zakariae.bouklah18@gmail.com>
2025-05-22 13:07:45 +0100

    feat: testing resources.

---
+++ src/test/resources/db/migrations/test-up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS apod (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    date TEXT NOT NULL UNIQUE,
+    explanation TEXT NOT NULL,
+    media_type TEXT NOT NULL,
+    copyright TEXT,
+    hdurl TEXT,
+    url TEXT
+);
