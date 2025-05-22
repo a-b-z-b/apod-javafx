c9865655e84c73b9d0b53bc25ee870cf87a5ccab
zZ7_aki <zakariae.bouklah18@gmail.com>
2025-05-22 14:39:08 +0100

    fix: return fresh db connection to avoid any thread-risks.

--- src/main/java/org/apod/data/DBConnection.java
+++ src/main/java/org/apod/data/DBConnection.java
@@ -4,26 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static Connection connection;
-
     public static Connection getConnection(String dbType) {
         try {
-            if (connection == null) {
-                switch (dbType){
-                    case "sqlite":
-                        Class.forName("org.sqlite.JDBC");
-                        connection = DriverManager.getConnection("jdbc:sqlite:data.db");
-                        break;
-                    case "test":
-                        Class.forName("org.sqlite.JDBC");
-                        return DriverManager.getConnection("jdbc:sqlite:data_test.db");
-                    default:
-                        throw new IllegalArgumentException("Unsupported DB");
-                }
+            switch (dbType){
+                case "sqlite":
+                    Class.forName("org.sqlite.JDBC");
+                    return DriverManager.getConnection("jdbc:sqlite:data.db");
+                case "test":
+                    Class.forName("org.sqlite.JDBC");
+                    return DriverManager.getConnection("jdbc:sqlite:data_test.db");
+                default:
+                    throw new IllegalArgumentException("Unsupported DB");
             }
         }catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return connection;
     }
 }
